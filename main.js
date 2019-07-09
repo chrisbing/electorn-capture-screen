1200361492d3b9f364420d6ecc6ef67fe414a014
chanble <chanble_cn@163.com>
2019-07-09 23:30:55 +0800

    创建窗口时加了nodeIntergration参数

--- main.js
+++ main.js
@@ -15,7 +15,13 @@ function createWindow() {
     useCapture()
 
     // 创建浏览器窗口。
-    win = new BrowserWindow({ width: 800, height: 600 })
+    win = new BrowserWindow({
+        width: 800,
+        height: 600,
+        webPreferences: {
+            nodeIntegration: true,
+        },
+    })
 
     // 然后加载应用的 index.html。
     win.loadFile('index.html')
